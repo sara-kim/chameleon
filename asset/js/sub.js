355563c9b50fb145e36fb1ac5a3e81f39bd1df97
MINJIN KIM <58720096+sara-kim@users.noreply.github.com>
2019-12-10 18:28:12 +0900

    css,html

--- asset/js/sub.js
+++ asset/js/sub.js
@@ -18,4 +18,30 @@ $(document).ready(function()
     });
     // FAQ 질문글 이벤트 종료
 
+    // Question 이벤트 시작
+    var _quesForm = $("#quesForm .selAreaWrap");
+    var _btn = _quesForm.find("button").first();
+    var _ulBtn = $("#quesForm .ulBtn");
+    
+    _btn.on("click",function(e)
+    {
+        e.preventDefault();
+        $(this).next().stop().slideDown();
+        // $(this).toggleClass("active");
+
+        $(this).next().on("mouseleave",function()
+        {
+            $(this).stop().slideUp();
+        });
+        _ulBtn.find("button:first, button:last").on("blur", function()
+        {
+            setTimeout(function()
+            {
+                if(!_ulBtn.find("button").is(":focus"))
+                _ulBtn.stop().slideUp(); 
+            },1000);
+        });
+        _quesForm.find("")
+    })
+    // Question 이벤트 종료
 });
